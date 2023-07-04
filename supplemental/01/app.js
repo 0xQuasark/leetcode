d0cb588c7fc63c9c74c24a19d8fc0992d4da2086
Quasark <q@quasark.xyz>
2023-07-03 20:50:08 -0700

    first exercise

---
+++ supplemental/01/app.js
@@ -0,0 +1,17 @@
+// Exercise:
+// Create an HTML page that displays a simple to-do list. The list should contain three tasks: 
+// "Buy groceries," "Do laundry," and "Go for a run." Each task should be displayed as a separate list item (<li> element). 
+// Apply the following styles using classes and ids:
+
+// Add a class to the <ul> element called "todo-list".
+// Apply a different class to each list item (<li>) element. You can name the classes anything you like.
+// Add an id to the second list item (<li>) element and name it "important-task".
+// Your task is to create the HTML structure and apply the appropriate classes and id. Feel free to style the list using CSS 
+// if you'd like, but that's optional for this exercise.
+
+// Once you've completed the exercise, let me know, and I'll provide feedback and move on to the next exercise based on your 
+// rating.
+
+
+
+
